b15601e21571c6773751a04aceaf380cb738b15b
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2024-12-23 15:53:00 +0100

    Update TBD_eleve.php

--- app/TBD_eleve.php
+++ app/TBD_eleve.php
@@ -10,57 +10,10 @@
         integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <script src="TBD_eleve.html"></script>
 </head>
+<?php require_once(__DIR__ . "/header.php");
+require_once(__DIR__ . "/aside.php");
 
-
-
-<header>
-    <nav class="haut">
-        <ul class="navbar">
-            <li>
-                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> Français
-                <img src="../IMG/icones/flèche.png" alt="Flèche" class="arrow" id="arrow">
-            </li>
-            <li id="english">
-                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> English
-            </li>
-        </ul>
-    </nav>
-</header>
-
-
-<aside class="sidenav">
-    <nav>
-        <img src="../IMG/USPN.png" alt="logo USPN" id="USPN" class="bouton-side">
-        <ul class="aside-ul">
-            <li>
-                <a href="bord.html" class="bouton-side" id="TDB">
-                    <div class="content-side">
-                        <img src="../IMG/icones/Dashboard.jpg" alt="Dashboard" id="Dashboard"> <span>Tableau de
-                            Bord</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-
-                <a href="document.html" class="bouton-side" id="DP">
-                    <div class="content-side">
-                        <i class="fas fa-folder" style="font-size: 1.3em;"></i> <span>Depot de document</span>
-                    </div>
-                </a>
-            </li>
-            <li>
-
-                <a href="#" class="bouton-side" id="Profil">
-                    <div class="content-side">
-                        <i class="fas fa-users" style="font-size: 1.3em;"></i><span>Profil</span>
-                    </div>
-
-                </a>
-            </li>
-        </ul>
-    </nav>
-    <a href="login.html" class="bouton-side" id="Deconecter"><i class="fas fa-sign-out-alt"></i> Déconnexion</a>
-</aside>
+?>
 
 <body>
 
