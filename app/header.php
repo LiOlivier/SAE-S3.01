b729b5069eb1dbc592a33e1e2b3adb386d8e22af
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2024-12-23 15:52:47 +0100

    header php

---
+++ app/header.php
@@ -0,0 +1,13 @@
+<header>
+    <nav class="haut">
+        <ul class="navbar">
+            <li>
+                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> Français
+                <img src="../IMG/icones/flèche.png" alt="Flèche" class="arrow" id="arrow">
+            </li>
+            <li id="english">
+                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> English
+            </li>
+        </ul>
+    </nav>
+</header>
