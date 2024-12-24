4ffd1618a16b81d05f4ea54be1acc82f516d9c3f
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2024-12-24 16:27:12 +0100

    design card etudiant

--- app/TBD_eleve.php
+++ app/TBD_eleve.php
@@ -15,8 +15,44 @@ require_once(__DIR__ . "/aside.php");
 
 ?>
 
-<body>
+<body class="body">
+    <section id="one">
+        <h1>Liste étudiants</h1>
 
+        <div class="card">
+        <div class="container">
+                            <div class="left">
+                                <div style="display: block;">
+                                <h3 class="nom">Maxime Lointier</h3>
+                                <h4 class="info">But2 Info </h4>
+                                <h4 class="info">Stymphale</h4>
+                                <button id="contacter">contacter</button>
+                                </div>
+                            </div>
+                            
+                            <div class="right" style="margin-top: 30px;">
+                                <div style="display: block;"><h6 style="margin-bottom: 0px;"> Etat</h6>
+                                <span style="font-size: 12px; margin-right: auto;"> Tache completé <i class="fas fa-circle" style="color: #63E6BE;"></i></span>
+                                </div>
+                                
+                            </div>
+                            
+                        </div>
+
+
+          
+
+        </div>
+
+
+    </section>
 </body>
 
 </html>
+
+<script>
+    // redirection vers la page de contact
+    document.getElementById("contacter").addEventListener("click", function() {
+        window.location.href = "contact.php";
+    });
+</script>
