326279dd6e819070309542b41d89742687f3f82a
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2024-12-29 16:12:24 +0100

    page depot creation

---
+++ app/depot.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Depôt des documents</title>
+    <link rel="stylesheet" href="../CSS/TBD.css">
+    <link rel="stylesheet" href="../CSS/aside.css">
+
+</head>
+<body>
+    
+</body>
+</html>
+<?php require_once(__DIR__ . "/header.php");
+require_once(__DIR__ . "/aside.php");
