af6f3c82aeabde83b8282d9ad69608dbcd894f49
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-01-02 16:44:44 +0100

    creation dossier component

---
+++ app/component/card_depot.php
@@ -0,0 +1,24 @@
+<div class="card ">
+    <div class="container">
+        <div class="left">
+            <div style="display: block;">
+                <h3 class="nom depot-nom">Bordereau de stage</h3>
+                <h4 class="date-limite"> Date limite : 12/01/2025</h4>
+                <h4 class="etat"> Etat</h4>
+                <div class="validation"> <i class="fas fa-circle" style="color: #63E6BE;"></i>Document Validé</div>
+
+                <form id="uploadForm" enctype="multipart/form-data">
+                    <button class="contacter">Modèle <i class="fas fa-download load" style="color: #c0c0c0;"></i></button>
+                    <input type="file" id="sortDocument" name="sortDocument" accept=".jpeg,.jpg,.png,.pdf" style="display:none;" />
+                    <button type="button" class="contacter joindre" onclick="document.getElementById('sortDocument').click()">
+                        Joindre fichier<i class="fas fa-upload load" style="color: #c0c0c0;"></i>
+                    </button>
+                </form>
+
+                <input type="hidden" value="email1@exemple.com">
+                <div id="notification-container" style="position: fixed; top: 20px; right: 20px; z-index: 9999;"></div>
+            </div>
+        </div>
+    </div>
+
+</div>
---
+++ app/component/notification.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="../CSS/notification.css">
+<div class="panel">
+    <h1 id="notif_title">Notification</h1>
+    <div class="notifs">
+        <div class="notif"> <h1 class="titre_n">Bordereau de stage </h1>
+        <div class="infor">Information precise</div>
+        <div class="date_recu"> 19/02/2001</div>
+    </div>
+    </div>
+</div>
