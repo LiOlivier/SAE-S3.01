5ba6db94e48801cb88d60ecfe5c34b90ad4b4e86
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-01-02 16:47:39 +0100

    bouton joindre fonctionelle

--- app/depot.php
+++ app/depot.php
@@ -9,42 +9,50 @@
     <link rel="stylesheet" href="../CSS/header.css">
     <link rel="stylesheet" href="../CSS/card.css">
     <link rel="stylesheet" href="../CSS/depot.css">
-
-
-
-
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
+        integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
 </head>
-<?php require_once(__DIR__ . "/header.php");
-require_once(__DIR__ . "/aside.php"); ?>
+<?php require_once(__DIR__ . "//component/header.php");
+require_once(__DIR__ . "//component/aside.php"); ?>
 
 <body class="body">
     <section id="one">
 
         <h1 id="titre">Document a déposer</h1>
         <div class="cards">
-            <h1>Contact : </h1>
-            <div class="card ">
-                <div class="container">
-                    <div class="left">
-                        <div style="display: block;">
-                            <h3 class="nom depot-nom">Bordereau de stage</h3>
-                            <h4 class="date-limite"> Date limite : 12/01/2025</h4>
-                            <h4 class="etat"> Etat</h4>
-                            <div class="validation"> <i class="fas fa-circle" style="color: #63E6BE;"></i>Document Validé</div>
-
-                            <button class="contacter" >Télécharger <i class="fas fa-download" style="color: #c0c0c0;"></i></button>
-                            <button class="contacter joindre " >Joindre fichier<i class="fas fa-upload" style="color: #c0c0c0;"></i></button>
-                            <input type="hidden" value="email1@exemple.com">
-                            <div id="notification-container" style="position: fixed; top: 20px; right: 20px; z-index: 9999;"></div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+            <?php require "component/card_depot.php"; ?>
         </div>
-
+        <?php require "component/notification.php" ?>
     </section>
 
 </body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="../JS/notif.js"></script>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $("#sortDocument").change(function() {
+            let formData = new FormData($("#uploadForm")[0]);
+
+            $.ajax({
+                url: "component/upload_handler.php",
+                type: "POST",
+                data: formData,
+                contentType: false,
+                processData: false,
+                dataType: "json" // Ajoutez cette ligne pour indiquer que la réponse attendue est du JSON
+            }).done(function(response) {
+                console.log(response);
+
+                if (response.status === "success") {
+                    showNotification("Fichier téléchargé avec succès.");
+                } else if (response.status === "error") {
+                    showNotification("Erreur: " + response.message, 5000); 
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                showNotification('Une erreur est survenue lors de l\'upload.', 5000);
+            });
+        });
+    });
+</script>
 
 </html>
