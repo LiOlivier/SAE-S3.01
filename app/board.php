689147fb0fe1c37209627b92f730624267760d88
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-01-02 16:46:32 +0100

    Update board.php

--- app/board.php
+++ app/board.php
@@ -9,16 +9,12 @@
     <link rel="stylesheet" href="../CSS/header.css">
     <link rel="stylesheet" href="../CSS/card.css">
     <link rel="stylesheet" href="../CSS/TBD.css">
-
-
-    
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
         integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <script src="TBD_eleve.html"></script>
 </head>
-<?php require_once(__DIR__ . "/header.php");
-require_once(__DIR__ . "/aside.php");
+<?php require_once(__DIR__ . "//component/header.php");
+require_once(__DIR__ . "//component/aside.php");
 
 ?>
 <style>
