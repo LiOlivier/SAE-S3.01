f542ee161272769290730b5a9f61ed77a948bf3f
Bharani <136856115+Bharani0426@users.noreply.github.com>
2025-01-10 18:21:34 +0100

    Update informatique.php

--- app/informatique.php
+++ app/informatique.php
@@ -12,4 +12,16 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
         integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <style>
+        .clickable-card {
+            text-align: center;
+            padding: 20px;
+            cursor: pointer;
+        }
+
+        .clickable-card .details {
+            font-size: 14px;
+            color: #666;
+        }
+    </style>
+</head>
 
