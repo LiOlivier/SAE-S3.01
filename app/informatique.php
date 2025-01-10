87280086fbd6801b2a58e19e3e8c69aad7cade4a
Bharani <136856115+Bharani0426@users.noreply.github.com>
2025-01-10 18:21:19 +0100

    Update informatique.php

---
+++ app/informatique.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BUT Informatique</title>
+    <link rel="stylesheet" href="../CSS/aside.css">
+    <link rel="stylesheet" href="../CSS/header.css">
+    <link rel="stylesheet" href="../CSS/card.css">
+    <link rel="stylesheet" href="../CSS/TBD.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
+        integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+    <style>
+
