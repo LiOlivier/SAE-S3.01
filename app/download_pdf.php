ede8f80cc971e35ce02cd0ba676c4ed13b8d9d50
Bharani <136856115+Bharani0426@users.noreply.github.com>
2025-01-16 16:40:09 +0100

    télécharger pdf

---
+++ app/download_pdf.php
@@ -0,0 +1,15 @@
+<?php
+// Spécifiez le chemin vers votre fichier PDF
+$filename = "path/to/your/pdf/mon_fichier.pdf";
+
+if (file_exists($filename)) {
+    header('Content-Type: application/pdf');
+    header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+    header('Content-Transfer-Encoding: binary');
+    header('Accept-Ranges: bytes');
+    readfile($filename);
+    exit;
+} else {
+    echo "Le fichier demandé n'existe pas.";
+}
+?>
