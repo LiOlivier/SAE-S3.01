ad29f4bb081ae49883a6e29a8f94eb8229b17c3e
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-01-16 15:10:31 +0100

    Create typeAction.php

---
+++ model/typeAction.php
@@ -0,0 +1,14 @@
+<?php
+require "../config/database.php";
+
+
+class TypeAction
+{
+    protected $table = "typeaction";
+    protected $db;
+
+    public function __construct($dbType = 'mysql')
+    {
+        $this->db = Database::getConnexion($dbType);
+    }
+}
