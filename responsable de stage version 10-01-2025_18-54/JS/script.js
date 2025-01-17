c9b4f11918be6438be2ba72cda190cb25619bc36
sherwin-jeremias <sherwin.jeremias@edu.univ-paris13.fr>
2025-01-17 17:51:02 +0100

    modif files

--- responsable de stage version 10-01-2025_18-54/JS/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("arrow").addEventListener("click", function() {
-    var english = document.getElementById("english");
-    if (english.style.display === "none") {
-        english.style.display = "block";
-    } else {
-        english.style.display = "none";
-    }
-});
