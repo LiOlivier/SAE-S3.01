020380dd2e957ca1dc049ab3f32701bd02f272c5
Yassafa <yassineessafa1@gmail.com>
2025-01-18 23:38:38 +0100

    Add files via upload

---
+++ app/tdb.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
+        integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../CSS/navHaut.css">
+    <link rel="stylesheet" href="../CSS/aside.css">
+    <link rel="stylesheet" href="../CSS/section.css">
+    <link rel="stylesheet" href="../CSS/listEtudiant.css">
+    
+    <title>Tableau de Bord</title>
+</head>
+<body>
+<?php 
+    require_once(__DIR__ ."/header.php");
+    require_once(__DIR__ ."/aside.php") ;
+    require_once(__DIR__ ."/section.php") ;
+?>
+<script src="../JS/newscript.js"></script>
+</body>
+</html>
---
+++ app/zoneFormation.php
@@ -0,0 +1,12 @@
+<?php require_once 'Modèle/info_card_chefdpt.php'; ?>
+<article id="article-tdb">
+    <h1 id="titre-formation">Formation</h1>
+    <div class="bloc-formation" data-file="listEtudiantS4">
+        <p id="titre-but">But 2 Informatique</p>
+        <p><?=$nb_etudiants_but2?> étudiants</p>
+    </div>
+    <div class="bloc-formation" data-file="listEtudiantS6">
+        <p id="titre-but">But 3 Informatique</p>
+        <p><?=$nb_etudiants_but3?> étudiants</p>
+    </div>
+</article>
