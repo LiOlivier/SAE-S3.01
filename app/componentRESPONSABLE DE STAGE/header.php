fd2c9606293fe0b842ab9a1bb19e19dc65f0d46a
sherwin-jeremias <sherwin.jeremias@edu.univ-paris13.fr>
2025-01-19 20:42:22 +0100

    Add files via upload

---
+++ app/componentRESPONSABLE DE STAGE/header.php
@@ -0,0 +1,20 @@
+<head>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
+        integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
+</head>
+<header>
+    <nav class="haut">
+        <ul class="navbar">
+            <li>
+                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> Français
+                <img src="../IMG/icones/flèche.png" alt="Flèche" class="arrow" id="arrow">
+            </li>
+            <li id="english">
+                <img src="../IMG/icones/planète.png" alt="Planète" class="icon"> English
+            </li>
+        </ul>
+    </nav>
+    <div class="nav_notif">
+        <div class="bell"> <i class="far fa-bell" style="cursor: pointer;"></i></div>
+    </div>
+</header>
