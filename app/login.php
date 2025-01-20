5efede604fe965f883d6053e807ea3c9c4848f27
Olivier <liolivier98@gmail.com>
2025-01-20 03:04:19 +0100

    modif login

--- app/login.php
+++ app/login.php
@@ -49,7 +49,6 @@ require_once "controller/loginController.php";
                         <input type="password" name="password" id="password" placeholder="Votre mot de passe">
                     </div>
                 </div>
-                <a href="#" class="mdp_oublie">Mot de passe oublié</a>
                 <input type="submit" value="connexion" name="connexion" class="but_login"></input>
             </form>
         </div>
