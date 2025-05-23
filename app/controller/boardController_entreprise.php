b6a2e6d3440acad639be904284543d9e1b9e12a0
DenisVuong <vuong.denis.p@gmail.com>
2025-05-23 19:34:53 +0200

    Mise à jour controller, suppression board_entreprise, ajout vue board_entrepriseView

--- app/controller/boardController_entreprise.php
+++ app/controller/boardController_entreprise.php
@@ -2,8 +2,8 @@
 session_start();
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
-require "../model/utilisateur.php";
-require "../model/typeAction.php";
+require "../models/TuteurEntrepriseModel.php";
+//require "../models/typeAction.php";
 
 if (!isset($_SESSION['user'])) {
     // Redirige vers la page de connexion si non authentifié
