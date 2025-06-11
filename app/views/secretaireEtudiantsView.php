70a1e305df3645690fe6194c9435471a650daf2e
sherwin-jeremias <sherwin.jeremias@edu.univ-paris13.fr>
2025-06-11 21:24:41 +0200

    Remove TBD.css and tableau.css

--- app/views/secretaireEtudiantsView.php
+++ app/views/secretaireEtudiantsView.php
@@ -7,9 +7,6 @@
     <title>Liste des Etudiants - Responsable de Stage</title>
     <link rel="stylesheet" href="../CSS/aside.css">
     <link rel="stylesheet" href="../CSS/header.css">
-    <link rel="stylesheet" href="../CSS/card.css">
-    <link rel="stylesheet" href="../CSS/TBD.css">
-    <link rel="stylesheet" href="../CSS/tableau.css">
     <link rel="stylesheet" href="../CSS/secretaire.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css"
         integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
