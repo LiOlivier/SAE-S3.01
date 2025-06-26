4579561acba467ef71af49e8f6052613d8219925
Maxime-Lointier <157472240+Maxime-Lointier@users.noreply.github.com>
2025-06-26 16:57:46 +0200

    fix doc lien

--- app/models/typeAction.php
+++ app/models/typeAction.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/../../config/database.php';
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
+
 class TypeAction
 {
     protected $table = "typeaction";
@@ -13,25 +14,28 @@ class TypeAction
         $this->db = Database::getConnexion($dbType);
     }
 
-     public function getActionByEnseignantId($userId) { 
-        
-        $sql = "SELECT libelle, Etat, lien_modele_doc, delai_limite, t.id_type_action, t.requiert_doc FROM $this->table t JOIN action a USING(id_type_action) WHERE a.id_Etudiant = :userId AND t.Executant = 'Etudiant'";
+    public function getActionByEnseignantId($userId)
+    {
+
+        $sql = "SELECT libelle, Etat, lien_modele_doc, delai_limite, t.id_type_action, t.requiert_doc, a.id_action FROM $this->table t JOIN action a USING(id_type_action) WHERE a.id_Etudiant = :userId AND t.Executant = 'Etudiant'";
         $query = $this->db->prepare($sql);
         $query->execute(['userId' => $userId]);
         return $query->fetchAll(PDO::FETCH_ASSOC);
-     }
-   
-    
-    public function getActionByEntreprise($id_Tuteur_Entreprise){  
+    }
+
+
+    public function getActionByEntreprise($id_Tuteur_Entreprise)
+    {
         $sql = "SELECT libelle, Etat, lien_modele_doc, t.id_type_action FROM $this->table t JOIN action a USING(id_type_action) WHERE a.id_Tuteur_Entreprise = :id_Tuteur_Entreprise AND t.Executant = 'Tuteur Pédagogique'";
         $query = $this->db->prepare($sql);
         $query->execute(['id_Tuteur_Entreprise' => $id_Tuteur_Entreprise]);
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-    
 
-    public function updateDocument($idAction, $libelle) {
-        $sql = "UPDATE action SET lien_document = :libelle, Etat = 'En attente' WHERE id_type_action = :idAction";
+
+    public function updateDocument($idAction, $libelle)
+    {
+        $sql = "UPDATE action SET lien_document = :libelle, Etat = 'En attente' WHERE id_action = :idAction";
         $query = $this->db->prepare($sql);
         $query->execute([
             'libelle' => $libelle,
@@ -39,12 +43,11 @@ class TypeAction
         ]);
     }
 
-    public  function selectActionById($idAction){
-        $sql = "SELECT lien_modele_doc FROM $this->table WHERE id_type_action = :idAction";
+    public  function selectActionById($idAction)
+    {
+        $sql = "SELECT lien_modele_doc FROM $this->table WHERE id_action = :idAction";
         $query = $this->db->prepare($sql);
         $query->execute(['idAction' => $idAction]);
         return $query->fetch(PDO::FETCH_ASSOC);
     }
-    
 }
-?>
